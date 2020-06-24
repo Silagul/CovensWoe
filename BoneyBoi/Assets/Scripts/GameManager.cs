746ea5e3aa9c8f52520edbb076facd194a032070
Saku Petlin <sakupetlin@kamk.fi>
2020-06-24 15:33:17 +0300

    Enabled Unity Analytics

--- BoneyBoi/Assets/Scripts/GameManager.cs
+++ BoneyBoi/Assets/Scripts/GameManager.cs
@@ -1,9 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private int score = 0;
+    private Scene thisScene;
+
+    private void Awake()
+    {
+        thisScene = SceneManager.GetActiveScene();
+        AnalyticsEvent.LevelStart(thisScene.name, thisScene.buildIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //Dictionary<string, object> customParams = new Dictionary<string, object>();
+            //customParams.Add("Score", score);
+            AnalyticsEvent.Custom("Score", new Dictionary<string, object>
+            {
+                {"score", score}
+            });
+            AnalyticsResult ar = AnalyticsEvent.Custom("Score");
+            Debug.Log("Result is " + ar.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            AddScore();
+        }
+
+    }
+
+    public void AddScore()
+    {
+        score++;
+        Debug.Log(score);
+    }
+
+
+
+
     public Vector3 GetVectorFromAngle(float angle)  //this is used to get vector3 form angle
     {
         float angleRad = angle * (Mathf.PI / 180f);
