7ed7b74e6aea52a588110fe935c22e57673fa53f
Saku Petlin <sakupetlin@kamk.fi>
2020-07-07 09:44:40 +0300

    Added SpikePit

---
+++ BoneyBoi/Assets/Scripts/World/SpikePit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikePit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Human")
+        {
+            collision.gameObject.GetComponent<Animator>().SetBool("Foothold", true);
+            collision.gameObject.GetComponent<Human>().SetState("Dead");
+        }
+    }
+}
